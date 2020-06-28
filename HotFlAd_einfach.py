18c7965a57b77ab716d4d2810d786acf25727254
Nasir Asadov <asadov.nasir@gmail.com>
2020-06-28 20:36:12 +0200

    eps-NTU class working, started creating import data plots.

--- HotFlAd_einfach.py
+++ HotFlAd_einfach.py
@@ -3,74 +3,40 @@ import numpy as np
 import pandas as pd
 from CoolProp.CoolProp import PropsSI
 import requests
-from os.path import join
-
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 '''
 Importing measurement data
 '''
 data_folder = '/Users/nasir/tubCloud/Shared/HotFlAd/03_Messdaten/Monitoring/data/8h'
-temp_path = join(data_folder, 'arrTemps.txt')
-MID_path = join(data_folder, 'arrMID.txt')
+temp_path = os.path.join(data_folder, 'arrTemps.txt')
+mid_path = os.path.join(data_folder, 'arrMID.txt')
+time_path = os.path.join(data_folder, 'arrTime.txt')
+q_path = os.path.join(data_folder, 'arrQ.txt')
+
 header_temp = ['bt'+str(i) for i in range(1, 20)]
-header_MID = ['mid'+str(i) for i in range(1, 7)]
+header_mid = ['mid'+str(i) for i in range(1, 7)]
+header_q = ['q'+str(i) for i in range(1, 7)]
+
 temp_data = pd.read_csv(temp_path, sep='\t', header=0, names=header_temp)
-MID_data = pd.read_csv(MID_path, sep='\t', header=0, names=header_MID)
-print(MID_data.head())
+mid_data = pd.read_csv(mid_path, sep='\t', header=0, names=header_mid)
+time_data = pd.read_csv(time_path)
+time_data['Europe/Berlin'] = pd.to_datetime(time_data['# Unix Time'], unit='s').astype('datetime64[ns, Europe/Berlin]')
+q_data = pd.read_csv(q_path, sep='\t', header=0, names=header_q)
+# print(temp_data.isnull().values.any())
+# print(MID_data.isnull().values.any())
+print(q_data.head())
 
-
-"""
-Initialization of the temperature sensors
-
-#t1 = 333.15  # [K] Temperature at the server HX inlet (pump box circuit)
-#t2 = 332.15  # [K] Temperature at the server HX outlet (pump box circuit)
-
-bt1 = 323.15  # [K] T at HT outlet (AdKM)
-dt_1_5_meas = -0.5
-bt5 = 322.65  # bt1 + dt_1_5_meas   # [K] T at HT outlet junction (real and sim server)
-dt_5_17_meas = -0.5
-bt17 = bt5 + dt_5_17_meas   # [K] Temperature at the buffer storage inlet
-dt_17_18_meas = 12
-bt18 = bt17 + dt_17_18_meas   # [K] Temperature at the buffer storage outlet
-dt_5_t3_meas = -0.5
-t3 = bt5 + dt_5_t3_meas   # [K] Temperature at the server HX inlet (HT circuit)
-dt_t3_t4_meas = 12
-t4 = t3 + dt_t3_t4_meas   # [K] Temperature at the server HX outlet (HT circuit)
-dt_t4_6_meas = -0.5
-bt6 = t4 + dt_t4_6_meas  # [K] T at HT inlet junction (real and sim server)
-dt_6_2_meas = -0.5
-bt2 = bt6 + dt_6_2_meas
-
-# bt2 = 328.15  # [K] T at HT inlet (AdKM)
-
-bt7 = 349.15  # [K] T at HX server-sim inlet (central heating circuit)
-bt8 = 339.15  # [K] T at HX server-sim outlet (central heating circuit)
-bt4 = 330.15  # [K] T at HX server-sim outlet (HT circuit)
-bt3 = 322.15  # [K] T at HX server-sim inlet (HT circuit)
-
-bt15 = 285.15  # [K] T re-cooling water in
-bt16 = 292.15  # [K] T re-cooling water out
-bt13 = 298.65  # [K] T re-cooled water in = T emulator mt out
-bt14 = 293.15  # [K] T re-cooled water out
-bt12 = 300.15  # [K] T emulator mt in
-
-bt0 = 291.15  # [K] T at LT inlet (AdKM)
-bt9 = 288.15  # [K] T at LT outlet (AdKM)
-bt11 = 288.65  # [K] T at LT inlet to KAE
-bt10 = 290.65  # [K] T at LT outlet from KAE
-"""
-
-"""
-Initiation of the volume flow rate sensors
-
-V_MID6 = 0.000167  # [m^3/s] Volume flow rate in the server circuit (10 l/min)
-V_MID5 = 0.000167  # [m^3/s] Volume flow rate in the server secondary circuit (10 l/min)
-V_MID4 = 0.000833  # [m^3/s] Volume flow rate in the server simulation circuit (50 l/min)
-V_MID3 = 0.000833  # [m^3/s] Volume flow rate in the server simulation circuit (50 l/min) (= V_BF1 ?)
-# V_BF1 = 0.000833  # [m^3/s] Volume flow rate at the adsorption chiller outlet (50 l/min)  # V_BF sensors not readable?
-# V_BF4 = 0.000833  # [m^3/s] Volume flow rate of the re-cooling water (50 l/min)
-V_MID2 = 0.001  # [m^3/s] Volume flow rate in the re-cooling circuit (60 l/min)
-V_MID1 = 0.000833  # [m^3/s] Volume flow rate in the cold water circuit (50 l/min)
-"""
+'''
+Plotting the input data
+'''
+f, ax = plt.subplots(figsize=(5, 6))
+sns.set_style("whitegrid")
+g = sns.lmplot(x=time_data['Europe/Berlin'], y=temp_data.bt1, data=temp_data, aspect=2)
+g = (g.set_axis_labels("Time [s]", "Temperature [K]").set(xlim=(0, len(time_data['Europe/Berlin'])), ylim=(0, 100)))
+plt.title("Temperature profiles")
+plt.show(g)
 
 
 dict_pipe_sizes = {25: {'ri': 0.0136, 'ro': 0.01685},  # DIN EN 10255
@@ -150,19 +116,25 @@ class Node:
         :param p:
         :param fluid:
         """
-        self.tc_in_meas = tc_in_meas
-        self.tc_out_meas = tc_out_meas
+        def c2k(t):
+            return t + 273.15
+
+        def lpm2cmps(v):
+            return v * 1.6667e-5
+
+        self.tc_in_meas = c2k(tc_in_meas)
+        self.tc_out_meas = c2k(tc_out_meas)
         self.dtc_meas = self.tc_out_meas - self.tc_in_meas
-        self.dVc_in = dvc_in
+        self.dVc_in = lpm2cmps(dvc_in)
         self.cpc_in = PropsSI('C', 'T', self.tc_in_meas, 'P', p, fluid)
         self.rhoc_in = PropsSI('D', 'T', self.tc_in_meas, 'P', p, fluid)
         self.dmc_in = self.rhoc_in * self.dVc_in
         self.Qc_meas = round(self.dmc_in * self.cpc_in * self.dtc_meas)
 
-        self.th_in_meas = th_in_meas
-        self.th_out_meas = th_out_meas
+        self.th_in_meas = c2k(th_in_meas)
+        self.th_out_meas = c2k(th_out_meas)
         self.dth_meas = self.th_in_meas - self.th_out_meas
-        self.dVh_in = dvh_in
+        self.dVh_in = lpm2cmps(dvh_in)
         self.cph_in = PropsSI('C', 'T', self.th_in_meas, 'P', p, fluid)
         self.rhoh_in = PropsSI('D', 'T', self.th_in_meas, 'P', p, fluid)
         self.dmh_in = self.rhoh_in * self.dVh_in
@@ -170,7 +142,7 @@ class Node:
 
 
 class EpsNtu(Node):
-    def __init__(self, ua, tc_in_meas, tc_out_meas, dvc_in, cpc_in, th_in_meas, th_out_meas, dvh_in, cph_in):
+    def __init__(self, ua, tc_in_meas, tc_out_meas, dvc_in, th_in_meas, th_out_meas, dvh_in):
         """
         This class calculates heat transfer and the output temperatures via epsilon-NTU method.
 
@@ -182,7 +154,7 @@ class EpsNtu(Node):
         :param th_out_meas:
         :param dvh_in:
         """
-        super().__init__(tc_in_meas, tc_out_meas, dvc_in, th_in_meas, th_out_meas, dvh_in, cpc_in, cph_in)
+        super().__init__(tc_in_meas, tc_out_meas, dvc_in, th_in_meas, th_out_meas, dvh_in)
         self.UA = ua
         self.dt_in = self.th_in_meas - self.tc_in_meas
         self.Cc = self.cpc_in * self.dmc_in
@@ -212,15 +184,16 @@ class TableResults(EpsNtu):
         :param dvh_in:
         """
         super().__init__(ua, tc_in_meas, tc_out_meas, dvc_in, th_in_meas, th_out_meas, dvh_in)
-        self.results_dict = {'T inlet': [self.tc_in_meas, self.th_in_meas],
-                             'T outlet': [self.tc_out_meas, self.th_out_meas],
+        self.results_dict = {'T inlet': [tc_in_meas, th_in_meas],
+                             'T outlet': [tc_out_meas, th_out_meas],
                              'Delta T': [self.dtc_meas, self.dth_meas],
-                             'Vol. fl. r. inlet': [self.dVc_in, self.dVh_in],
+                             'Vol. fl. r. inlet': [dvc_in, dvh_in],
                              'Cp inlet': [self.cpc_in, self.cph_in],
                              'Rho inlet': [self.rhoc_in, self.rhoh_in],
                              'Mass fl. r.': [self.dmc_in, self.dmh_in],
                              'Simulation UA value': [self.UA, self.UA],
                              'Measured heat transfer': [self.Qc_meas, self.Qh_meas],
+                             'Imported heat transfer': [q_data.q1[sr], q_data.q2[sr]],
                              'Simulated heat transfer': [self.Q_sim, self.Q_sim]}
         self.columns = list(self.results_dict.keys())
         self.index = ['Cold side', 'Hot side']
@@ -230,7 +203,9 @@ class TableResults(EpsNtu):
 """
 Sandbox
 """
-emul_df = TableResults(650, temp_data.bt11, temp_data.bt10, MID_data.mid1, temp_data.bt12, temp_data.bt13, MID_data.mid2)
+sr = 2000  # Sample row number
+emul_df = TableResults(450, temp_data.bt10[sr], temp_data.bt11[sr], mid_data.mid1[sr], temp_data.bt12[sr],
+                       temp_data.bt13[sr], mid_data.mid2[sr])
 print(emul_df.results_df)
 #serv_df = TableResults(3250, t3, t4, mid6, t1, t2, mid5)
 #print(serv_df.results_df)
