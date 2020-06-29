7cb605722152970bbb0655fa427062e6a9c6cdcb
Nasir Asadov <asadov.nasir@gmail.com>
2020-06-29 14:08:36 +0200

    Added visualization of eps-NTU model validation.

--- HotFlAd_einfach.py
+++ HotFlAd_einfach.py
@@ -1,4 +1,4 @@
-from math import exp, pi, log
+from math import exp, pi, log, sqrt
 import numpy as np
 import pandas as pd
 from CoolProp.CoolProp import PropsSI
@@ -9,11 +9,13 @@ import seaborn as sns
 '''
 Importing measurement data
 '''
-data_folder = '/Users/nasir/tubCloud/Shared/HotFlAd/03_Messdaten/Monitoring/data/8h'
-temp_path = os.path.join(data_folder, 'arrTemps.txt')
-mid_path = os.path.join(data_folder, 'arrMID.txt')
-time_path = os.path.join(data_folder, 'arrTime.txt')
-q_path = os.path.join(data_folder, 'arrQ.txt')
+monitoring_folder = '/Users/nasir/tubCloud/Shared/HotFlAd/03_Messdaten/Monitoring'
+data_folder = os.path.join(monitoring_folder, 'data')
+data_8h = os.path.join(data_folder, '8h')
+temp_path = os.path.join(data_8h, 'arrTemps.txt')
+mid_path = os.path.join(data_8h, 'arrMID.txt')
+time_path = os.path.join(data_8h, 'arrTime.txt')
+q_path = os.path.join(data_8h, 'arrQ.txt')
 
 header_temp = ['bt'+str(i) for i in range(1, 20)]
 header_mid = ['mid'+str(i) for i in range(1, 7)]
@@ -27,38 +29,10 @@ q_data = pd.read_csv(q_path, sep='\t', header=0, names=header_q)
 # print(temp_data.isnull().values.any())
 # print(MID_data.isnull().values.any())
 
+
 '''
-Plotting the input data
-
-sns.set(style="whitegrid")
-
-fig, ax = plt.subplots()
-x = temp_data['timestamp']
-y1 = temp_data['bt10']
-y2 = temp_data['bt11']
-y3 = temp_data['bt12']
-y4 = temp_data['bt13']
-ax.set_xticklabels(temp_data['timestamp'], rotation=45, ha="right")
-
-ax.plot(x, y1, label='bt10')
-ax.plot(x, y2, label='bt11')
-ax.plot(x, y3, label='bt12')
-ax.plot(x, y4, label='bt13')
-ax.legend(loc='best')
+Duct sizes dictionary for heat loss calculation
 '''
-'''
-temp_data['timestamp'] = time_data['Europe/Berlin']
-#temp_data.set_index('timestamp', inplace=True)
-duration = [*range(1, 26370)]
-print(duration[-1])
-
-for i in range(1, 20, 1):
-    sns.lineplot(x=duration, y=temp_data.iloc[:, i], data=temp_data)
-set_axis_labels("Time [s]", "Temperature [K]").set(xlim=(0, len(time_data['Europe/Berlin'])), ylim=(0, 100))
-plt.title("Temperature profiles")
-plt.show(g)
-'''
-
 dict_pipe_sizes = {25: {'ri': 0.0136, 'ro': 0.01685},  # DIN EN 10255
                    32: {'ri': 0.01795, 'ro': 0.0212},
                    40: {'ri': 0.0209, 'ro': 0.02415}}
@@ -149,7 +123,7 @@ class Node:
         self.cpc_in = PropsSI('C', 'T', self.tc_in_meas, 'P', p, fluid)
         self.rhoc_in = PropsSI('D', 'T', self.tc_in_meas, 'P', p, fluid)
         self.dmc_in = self.rhoc_in * self.dVc_in
-        self.Qc_meas = round(self.dmc_in * self.cpc_in * self.dtc_meas)
+        self.Qc_calc = round(self.dmc_in * self.cpc_in * self.dtc_meas)
 
         self.th_in_meas = c2k(th_in_meas)
         self.th_out_meas = c2k(th_out_meas)
@@ -158,7 +132,7 @@ class Node:
         self.cph_in = PropsSI('C', 'T', self.th_in_meas, 'P', p, fluid)
         self.rhoh_in = PropsSI('D', 'T', self.th_in_meas, 'P', p, fluid)
         self.dmh_in = self.rhoh_in * self.dVh_in
-        self.Qh_meas = round(self.dmh_in * self.cph_in * self.dth_meas)
+        self.Qh_calc = round(self.dmh_in * self.cph_in * self.dth_meas)
 
 
 class EpsNtu(Node):
@@ -212,8 +186,8 @@ class TableResults(EpsNtu):
                              'Rho inlet': [self.rhoc_in, self.rhoh_in],
                              'Mass fl. r.': [self.dmc_in, self.dmh_in],
                              'Simulation UA value': [self.UA, self.UA],
-                             'Measured heat transfer': [self.Qc_meas, self.Qh_meas],
-                             'Imported heat transfer': [q_data.q1[sr], q_data.q2[sr]],
+                             'Measured heat transfer': [self.Qc_calc, self.Qh_calc],
+                             'Imported heat transfer': [q_data.q1[sr]*1000, None],
                              'Simulated heat transfer': [self.Q_sim, self.Q_sim]}
         self.columns = list(self.results_dict.keys())
         self.index = ['Cold side', 'Hot side']
@@ -223,11 +197,61 @@ class TableResults(EpsNtu):
 """
 Sandbox
 """
-sr = 7000  # Sample row number
-emul_df = TableResults(450, temp_data.bt10[sr], temp_data.bt11[sr], mid_data.mid1[sr], temp_data.bt12[sr],
+sr = 3000  # Sample row number
+ua = 350
+emul_df = TableResults(ua, temp_data.bt9[sr], temp_data.bt17[sr], mid_data.mid1[sr], temp_data.bt12[sr],
                        temp_data.bt13[sr], mid_data.mid2[sr])
-print(emul_df.results_df)
+#print(emul_df.results_df)
 #serv_df = TableResults(3250, t3, t4, mid6, t1, t2, mid5)
 #print(serv_df.results_df)
 #recooler_df = TableResults(3500, bt15, bt16, V_BF4, bt13, bt14, mid2)
 #print(recooler_df.results_df)
+
+
+'''
+Plotting the input data
+
+temp_data['timestamp'] = np.linspace(1, len(temp_data.bt1), len(temp_data.bt1))
+fig, ax = plt.subplots()
+x = temp_data['timestamp']
+y1 = temp_data['bt10']
+y2 = temp_data['bt11']
+y3 = temp_data['bt12']
+y4 = temp_data['bt13']
+
+ax.plot(x, y1, label='bt10')
+ax.plot(x, y2, label='bt11')
+ax.plot(x, y3, label='bt12')
+ax.plot(x, y4, label='bt13')
+ax.legend(loc='best')
+plt.xlim(sr-600, sr+600)
+plt.show()
+'''
+qsim_list = [round(EpsNtu(ua, temp_data.bt9[i], temp_data.bt17[i], mid_data.mid1[i], temp_data.bt12[i],
+             temp_data.bt13[i], mid_data.mid2[i]).Q_sim) for i in range(0, len(temp_data.bt1))]
+qcalc_list = [round(EpsNtu(ua, temp_data.bt9[i], temp_data.bt17[i], mid_data.mid1[i], temp_data.bt12[i],
+             temp_data.bt13[i], mid_data.mid2[i]).Qc_calc) for i in range(0, len(temp_data.bt1))]
+q_data['timestamp'] = np.linspace(1, len(q_data.q1), len(q_data.q1))
+q_data['kk_sim'] = qsim_list
+q_data['kk_calc'] = qcalc_list
+fig, ax = plt.subplots()
+x = q_data.timestamp
+y1 = q_data.q1*1000
+y2 = q_data.kk_sim
+y3 = q_data.kk_calc
+
+q_data['rmse'] = np.sqrt(abs(q_data.q1**2 - q_data.kk_sim**2))
+rmse = int(q_data.rmse.mean())
+ax.plot(x, y1, label='imported', linewidth=1)
+ax.plot(x, y2, label='eps-ntu', linewidth=1)
+ax.plot(x, y3, label='calculated', linewidth=1)
+plt.legend(loc='best')
+plt.xlim(sr-600, sr+600)
+#plt.xlim(0, len(q_data.q1))
+plt.ylim(1000, 15000)
+plt.xlabel('Time [s]')
+plt.ylabel('Qdot [W]')
+plt.title('Qdot over bt9-17 (UA = {}, RMSE = {})'.format(ua, rmse))
+plt.grid('on')
+#fig.savefig(data_folder+'/Qdot_bt9bt17_ua{}_rmse{}'.format(ua, rmse)+'.png', dpi=600)
+plt.show()
